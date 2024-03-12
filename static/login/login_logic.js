628bf27f6c8c388dba2646adb03de3eba773ec43
Yim Jiun Jye <jiunjye.yim@gmail.com>
2024-03-12 21:24:40 +0800

    Added: Registration form

--- static/login/login_logic.js
+++ static/login/login_logic.js
@@ -1,84 +1,98 @@
 const form = document.getElementById('sendOTP');
 document.addEventListener('DOMContentLoaded', function () {
-
-  form.addEventListener('submit', sending_OTP);
+    form.addEventListener('submit', sending_OTP);
 });
 
+const registerBtn = document.getElementById("register-btn");
+registerBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'inline';
+});
+
+const loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById('login-form').style.display = 'inline';
+    document.getElementById('register-form').style.display = 'none';
+});
+
+
 function sending_OTP(event) {
-  const apiUrl = 'http://127.0.0.1:8000/api/auth_user/send_otp/';
-  event.preventDefault();
-  // Using Fetch API to send a POST request
-  fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email: form.elements['email'].value,
-      password: form.elements['password'].value,
-    }),
-  })
+    const apiUrl = 'http://127.0.0.1:8000/api/auth_user/send_otp/';
+    event.preventDefault();
+    // Using Fetch API to send a POST request
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        email: form.elements['email-login'].value,
+        password: form.elements['password-login'].value,
+        }),
+    })
     .then(response => {
-      // Check if the response status is OK (status code 200-299)
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json(); // Parse the JSON in the response
+        // Check if the response status is OK (status code 200-299)
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // Parse the JSON in the response
     })
     .then(data => {
-      // Handle the data from the response
-      console.log('Data from server:', data);
-      console.log('detail:', data['detail'])
-      divInput = document.createElement('div');
-      divInput.setAttribute('class', 'input-box')
-      otpInput = document.createElement('input');
-      otpInput.setAttribute('type', 'text')
-      otpInput.setAttribute('id', 'otp')
-      otpInput.setAttribute('name', 'otp')
-      otpInput.setAttribute('placeholder', 'Enter OTP')
-      otpInput.setAttribute('required', '')
-      const login_fields = document.getElementById('login-input-fields');
-      divInput.appendChild(otpInput)
-      login_fields.appendChild(divInput);
-      // form.innerHTML = "Login with OTP";
-      form.removeEventListener("submit", sending_OTP);
-      form.addEventListener("submit", login_OTP);
+        // Handle the data from the response
+        console.log('Data from server:', data);
+        console.log('detail:', data['detail'])
+        divInput = document.createElement('div');
+        divInput.setAttribute('class', 'input-box')
+        otpInput = document.createElement('input');
+        otpInput.setAttribute('type', 'text')
+        otpInput.setAttribute('id', 'otp')
+        otpInput.setAttribute('name', 'otp')
+        otpInput.setAttribute('placeholder', 'Enter OTP')
+        otpInput.setAttribute('required', '')
+        const login_fields = document.getElementById('login-input-fields');
+        divInput.appendChild(otpInput)
+        login_fields.appendChild(divInput);
+        // form.innerHTML = "Login with OTP";
+        form.removeEventListener("submit", sending_OTP);
+        form.addEventListener("submit", login_OTP);
     })
     .catch(error => {
-      // Handle any errors that occurred during the fetch
-      console.error('Fetch error:', error);
+        // Handle any errors that occurred during the fetch
+        console.error('Fetch error:', error);
     });
 };
 
 function login_OTP(event) {
-  const apiUrl = 'http://127.0.0.1:8000/api/auth_user/login/';
-  event.preventDefault();
-  // Using Fetch API to send a POST request
-  fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email: form.elements['email'].value,
-      password: form.elements['password'].value,
-      otp: form.elements['otp'].value,
-    }),
-  })
+    const apiUrl = 'http://127.0.0.1:8000/api/auth_user/login/';
+    event.preventDefault();
+    // Using Fetch API to send a POST request
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        email: form.elements['email'].value,
+        password: form.elements['password'].value,
+        otp: form.elements['otp'].value,
+        }),
+    })
     .then(response => {
-      // Check if the response status is OK (status code 200-299)
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json(); // Parse the JSON in the response
+        // Check if the response status is OK (status code 200-299)
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // Parse the JSON in the response
     })
     .then(data => {
-      // Handle the data from the response
-      console.log(data)
+        // Handle the data from the response
+        console.log(data)
     })
     .catch(error => {
-      // Handle any errors that occurred during the fetch
-      console.error('Fetch error:', error);
+        // Handle any errors that occurred during the fetch
+        console.error('Fetch error:', error);
     });
 };
 
