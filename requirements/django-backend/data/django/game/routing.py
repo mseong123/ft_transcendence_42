2c15d8c40fa82af6f1d90cab5ac39351635b250b
Yim Jiun Jye <jiunjye.yim@gmail.com>
2024-04-20 15:56:01 +0800

    Edit: Websocket URI to start with /ws for NGINX implementation

--- requirements/django-backend/data/django/game/routing.py
+++ requirements/django-backend/data/django/game/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"game/(?P<room_name>[\w-]+)/$", consumers.GameLobbyConsumer.as_asgi()),
-	re_path(r"game/active/(?P<room_name>[\w-]+)/$", consumers.GameConsumer.as_asgi()),
+    re_path(r"ws/game/(?P<room_name>[\w-]+)/$", consumers.GameLobbyConsumer.as_asgi()),
+	re_path(r"ws/game/active/(?P<room_name>[\w-]+)/$", consumers.GameConsumer.as_asgi()),
 ]
